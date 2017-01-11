1389ae2c09ee2c554e39377b92d03e29dcde33c3
yangxiangming <yangxiangming@live.com>
2017-01-11 17:32:00 +0800

    测试模型

--- python.coding/pythonsite/appsite/models.py
+++ python.coding/pythonsite/appsite/models.py
@@ -2,4 +2,11 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Poll(models.Models):
+    question = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+class Choice(models.Models):
+    poll = models.ForeignKey(Poll)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
