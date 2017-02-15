29139e148cef8e9dbcb489c19c69cf91f0ad7f7f
yangxiangming <yangxiangming@live.com>
2017-02-15 09:46:07 +0800

    测试

--- python.coding/develop.py
+++ python.coding/develop.py
@@ -89,3 +89,21 @@ print "你输入的内容是: ", str
 
 import mysql.connector
 print mysql.connector.__version__
+
+h = 0
+leap = 1
+from math import sqrt
+from sys import stdout
+for m in range(101,201):
+    k = int(sqrt(m + 1))
+    for i in range(2,k + 1):
+        if m % i == 0:
+            leap = 0
+            break
+    if leap == 1:
+        print '%-4d' % m
+        
+        if h % 10 == 0:
+            print ''
+    leap = 1
+print 'The total is %d' % h
