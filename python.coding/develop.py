c39fd0986b1a6655797e9ba27d6cb53bb9910740
杨祥明 <yangxiangming@live.com>
2017-02-24 10:36:17 +0800

    测试
    测试

--- python.coding/develop.py
+++ python.coding/develop.py
@@ -124,3 +124,21 @@ if (year % 400 == 0) or ((year % 4 == 0) and (year % 100 != 0)):
 if (leap == 1) and (month > 2):
     sum += 1
 print 'it is the %dth day.' % sum
+
+x = int(raw_input("请输入一个数:\n"))
+a = x / 10000
+b = x % 10000 / 1000
+c = x % 1000 / 100
+d = x % 100 / 10
+e = x % 10
+
+if a != 0:
+    print "5 位数：",e,d,c,b,a
+elif b != 0:
+    print "4 位数：",e,d,c,b,
+elif c != 0:
+    print "3 位数：",e,d,c
+elif d != 0:
+    print "2 位数：",e,d
+else:
+    print "1 位数：",e 
